12a33025b8abd3530b666285e34d65caa6bcf41e
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2022-07-17 21:17:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import QrCode from "./Components/QrCode"
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="section container">
+      <QrCode />
+    </div>
+  );
+}
+
+export default App;
